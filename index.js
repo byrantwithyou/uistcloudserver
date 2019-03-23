8bfe24b7f58f9875a45fec3d7f4342d107b87a85
byrantwithyou <byrantwithyou@163.com>
2019-03-22 22:08:09 -0400

    dsf

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
-
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, '');
-
 const server = express()
   .use(express.static(path.join(__dirname, "public")))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 const io = socketIO(server);
 
-//const INDEX = path.join(__dirname, "index.html");
-
-//const server = express().use((req, res) => res.sendFile(INDEX)).listen(port, () => console.log());
 
 let studentProfile = [];
 let behaviorsForAll = [];
@@ -21,7 +14,6 @@ let stepsForAll = [];
 let subsectionsForAll = [];
 let settingsForAll = [];
 let teacherID = "";
-let styleProfile = [];
 let stepProfile = [];
 
 
@@ -50,19 +42,6 @@ io.on("connection", function (socket) {
   })
 
   socket.on("styleData", function(style, name, result) {
-    // console.log(data);
-    // let find = false;
-    // for (let profile of styleProfile) {
-    //   if ( (profile.style == data.style) && (profile.name == data.name)) {
-    //     profile.reviewResult = data.reviewResult;
-    //     find = true;
-    //     break;
-    //   }
-    // }
-    // if (!find) {
-    //   find = false;
-    //   styleProfile.push(data);
-    // }
     
     if (teacherID && io.sockets.connected[teacherID]) {
       io.sockets.connected[teacherID].emit("styleLog", style, name, result);
@@ -71,37 +50,9 @@ io.on("connection", function (socket) {
   })
 
 
-  socket.on("failureHistory", function(behavior) {
-    if (teacherID && io.sockets.connected[teacherID]) {
-      io.sockets.connected[teacherID].emit("failureHistory", studentProfile.find((element) => (element.id == socket.id)).name, behavior);
-    }
-  })
 
-  // socket.on("stepAction", function(behavior) {
-  //   if (behavior) {
-  //     if (studentProfile.find((element) => (element.id == socket.id)).behavior[behavior.name]) {
-  //       studentProfile.find(element => element.id == socket.id).behavior[behavior.name] += 1;
-  //     } else {
-  //       studentProfile.find((element) => (element.id == socket.id)).behavior[behavior.name] = 1;
-  //     }
-  //     if (
-  //       studentProfile.find(element => element.id == socket.id) &&
-  //       studentProfile.find(element => element.id == socket.id).online
-  //     ) {
-  //       io.sockets.connected[socket.id].emit(
-  //         "behaviorProfile",
-  //         studentProfile.find(element => element.id == socket.id).behavior
-  //       );
-  //     }
-  //   }
-  //   if (teacherID && io.sockets.connected[teacherID]) {
-  //     io.sockets.connected[teacherID].emit(
-  //       "stepAction",
-  //       studentProfile.find(element => element.id == socket.id).name,
-  //       behavior
-  //     );
-  //   }
-  // })
+
+
 
   socket.on("pr", function(studentName) {
     const index = studentProfile.findIndex(element => element.name == studentName);
@@ -148,30 +99,31 @@ io.on("connection", function (socket) {
 
   //when a student login
   socket.on("studentLogin", function (studentName) {
+    if (behaviorsForAll.length == 0) {
+      if (io.sockets.connected[socket.id]) {
+        socket.emit("notAuthored");
+        return;
+      }
+      return;
+    }
     if (!studentProfile.map((element) => (element.name)).includes(studentName)) {
       studentProfile.push({
         id: socket.id,
         name: studentName,
         step: 1,
-        behavior: {},
-        online: true,
         stepContent: subsectionsForAll[0].steps[0],
         currentSection: 1
       });
-    } 
-    stepProfile.push({
-      id: socket.id,
-      name: studentName,
-      currentSubsection: "",
-      stepContent: ""
-    });
+    } else {
+      studentProfile[studentProfile.findIndex((element) => (element.name == studentName))].id = socket.id;
+    }
     
-    socket.emit("authoring", behaviorsForAll, stepsForAll, subsectionsForAll, settingsForAll, studentProfile.find((element) => (element.name)).step);
+    if (io.sockets.connected[socket.id]) {
+      socket.emit("authoring", behaviorsForAll, stepsForAll, subsectionsForAll, settingsForAll, studentProfile.find((element) => (element.name == studentName)).step);
+    }
+    
     if (teacherID && io.sockets.connected[teacherID]) {
       io.sockets.connected[teacherID].emit("studentProfile", studentProfile.map((element) => ([element.name, element.step])));
-    }
-    if (teacherID && io.sockets.connected[teacherID]) {
-      io.sockets.connected[teacherID].emit("stepProfile", stepProfile);
     }
     if ( teacherID && io.sockets.connected[teacherID]) {
       io.sockets.connected[teacherID].emit("studentLogin", studentName);
@@ -244,32 +196,9 @@ io.on("connection", function (socket) {
     }
   });
   
-  socket.on("broadcast", function(message) {
-    for (let student of studentProfile) {
-      io.sockets.connected[student.id].emit("broadcast", message);
-    }
-  })
 
-  socket.on("review", function (review, img, behavior, toUser, text, severity) {
-    
 
-    //if review is positive
-    if (review == 1) {
-      io.sockets.connected[toUser].emit("reviewResult", 1, behavior);
-    }
-    //else if review is negative
-    else if (review === 0) {
-      io.sockets.connected[toUser].emit("reivewResult", 0, behavior);
-    }
-  });
 
-  // a student wants to get review from teacher
-  socket.on("photoToTeacher", function (img, behavior, severity) {
-    let target = teacherSocket;
-    if (target) {
-      target.emit("photoToJudgeByTeacher", img, behavior, socket.id, severity);
-    }
-  });
 
   socket.on("reviewResult", function(reviewResult, reviewStudentName, reviewBehavior, reviewComment, reviewImg) {
     //console.log(studentProfile[0].id);
@@ -291,53 +220,7 @@ io.on("connection", function (socket) {
     }
   })
 
-  // teacher sends back review
-  socket.on("teacherReview", function (
-    review,
-    img,
-    behavior,
-    toUser,
-    text,
-    severity
-  ) {
-    //if review is positive
-    if (review === 1) {
-      
-    }
-    //else if review is negative
-    else if (review === 0) {
-      //when facing an error, student should get three positive reviews continuously to develop his behavior
-      
-    }
 
-    //if severity is 1, store the review instead of sending it
-    if (severity === "low") {
-      let finalMsg = {};
-      finalMsg.toUser = toUser;
-      finalMsg.status = review;
-      finalMsg.behavior = behavior;
-      finalMsg.text = text;
-    } else {
-      let target =
-        studentProfile[
-          studentProfile.findIndex(element => element.id === toUser)
-        ].socket;
-      target.emit("reviewResult", img, review, behavior, text, severity);
-    }
-
-    //If the behavior is formed, tell the student
-    //There is still a 30 percent chance to check the answer
-    if (
-      studentProfile[studentProfile.findIndex(element => element.id === toUser)]
-        .errorRecord[behavior] === -2
-    ) {
-      let target =
-        studentProfile[
-          studentProfile.findIndex(element => element.id === toUser)
-        ].socket;
-      target.emit("behaviorDeveloped", behavior);
-    }
-  });
 
   socket.on("review2Teacher", function(reviewResultImg, reviewResultBehavior, studentName) {
     if (teacherID && io.sockets.connected[teacherID]) {
